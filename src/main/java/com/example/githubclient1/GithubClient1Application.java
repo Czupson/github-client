d071e914b0e8d22a2af883fb8e3b6b70ff00d6b3
Mateusz Czuper <mateusz300102@gmail.com>
2026-09-03 14:49:02 +0200

    first commit

---
+++ src/main/java/com/example/githubclient1/GithubClient1Application.java
@@ -0,0 +1,13 @@
+package com.example.githubclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubClient1Application.class, args);
+	}
+
+}
